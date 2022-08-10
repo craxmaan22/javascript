010f2b46a81942462aacef570c7f6e7debf4ff3d
craxmaan22 <109687512+craxmaan22@users.noreply.github.com>
2022-08-10 16:13:39 -0700

    function progress

--- js/index.js
+++ js/index.js
@@ -1,23 +1,29 @@
 window.onload = function() {
-   
-  window.onload = function() {
-    let form = document.querySelector("form");
-    form.onsubmit = function(event) {
-      event.preventDefault();
-      const shoutInput = document.getElementById("shoutInput").value;
-      
-    // new line below
-  
-  
-  
-  // const caps = function(phrase) {                     
-  //   return phrase.toUpperCase();
-  // } 
-  //     console.log(caps);
-    document.querySelector("span#shout").innerText = shoutInput;
-  
-      document.querySelector("div#allCaps").removeAttribute("class");
-      const shout = document.getElementById("shout").string;
- };
-  
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    
+    let upperCase = document.getElementById("shoutInput").value;
+    let result = upperCase.toUpperCase();
+    document.getElementById("shout").innerHTML = result;
+    console.log(result);
+
+    function upperCase() {
+      let upperCase text = "Hello world";
+     
+        }
+    
+
+
+  // new line below
+
+// const caps = function(phrase) {                     
+//   return phrase.toUpperCase();
+// } 
+//     console.log(caps);
+  document.querySelector("span#shout").innerText = shoutInput;
+  document.querySelector("div#allCaps").removeAttribute("class");
+
   };
+
+};
